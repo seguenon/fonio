dbab0b810d5c834a128d5f6333e67189dce8c37a
seguenon <seguenonkone02@gmail.com>
2022-10-14 23:56:35 +0000

    envoi du projet sur git

---
+++ Formation_Dev/Template_youtube/assets/js/script.js
@@ -0,0 +1,19 @@
+window.onload=()=>{
+    var collapse=document.getElementById('collapse')
+    var menu=document.getElementById('menu')
+    if(collapse){
+        collapse.addEventListener('click',()=>{
+            if(menu.style.display==="block"){
+                menu.style.display="none"
+            }
+            else{
+                menu.style.display="block"
+            }
+        })
+        if(menu){
+            menu.addEventListener('mouseleave',()=>{
+                menu.style.display="none"
+            })
+        }
+    }
+}
